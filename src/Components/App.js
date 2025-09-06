002c1d5c7be57dcec1dd2b3101d2cfc37f323676
SaeedKarimian860 <SGK12190@gmail.com>
2025-09-06 13:43:35 -0400

    react quiz

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
-
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -47,8 +47,8 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       };
-    case "nextQuestion": 
-      return {...state, index: state.index + 1};
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
 
     default:
       throw new Error("Action unknown");
@@ -56,12 +56,16 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -82,12 +86,19 @@ export default function App() {
         )}
         {status === "active" && (
           <>
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
-          <NextButton dispatch={dispatch} answer={answer} />
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
           </>
         )}
       </Main>
