db8d1f435483af11b74991c61a02cbbdf65f4779
SaeedKarimian860 <SGK12190@gmail.com>
2025-09-07 18:21:21 -0400

    react quiz

--- src/Components/FinishScreen.js
+++ src/Components/FinishScreen.js
@@ -6,6 +6,7 @@ function FinishScreen({ points, maxPossiblePoints, highscore }) {
   if (percentage >= 80 && percentage < 100) emoji = "🎉";
   if (percentage >= 50 && percentage < 80) emoji = "🙃";
   if (percentage >= 0 && percentage < 50) emoji = "🤔";
+  
   if (percentage === 0) emoji = "🤦‍♂️";
 
   return (
