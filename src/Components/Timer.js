cef5d664ba27a45109c233feb4c764608f1842eb
SaeedKarimian860 <SGK12190@gmail.com>
2025-09-21 11:06:02 -0400

    react quiz

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -18,6 +18,7 @@ function Timer({ dispatch, secondsRemaining }) {
   return (
     <div className="timer">
       {mins < 10 && "0"}
+      
       {mins}:{seconds < 10 && "0"}
       {seconds}
     </div>
